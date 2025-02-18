0cbb57f7aecc4bdb3fa4375ee71ed089f81dccfd
Md.Mehedi Hassan <mehedihassan4467@gmail.com>
2025-02-18 22:34:22 +0600

    logo and background image added and modify form card

---
+++ src/components/custom/Common/Spinner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className={`spinner-wrapper`}>
+      <div>
+        <div className="spinner">
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+          <div className="spinner-blade"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
